6c2fb7ec8c909351edd7e33b58bb1fccaaef5541
Mathieu Virbel <txprog@gmail.com>
2010-02-19 01:03:11 +0100

    add /factory/list + /factory/describe?name=

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -137,6 +137,89 @@ void web_pipeline_status() {
 	free(out);
 }
 
+void web_factory_list() {
+	char *out;
+	std::vector<std::string>::iterator it;
+	std::vector<std::string> list = otFactory::getInstance()->list();
+	cJSON *root, *data;
+
+	root = cJSON_CreateObject();
+	cJSON_AddNumberToObject(root, "success", 1);
+	cJSON_AddStringToObject(root, "message", "ok");
+	cJSON_AddItemToObject(root, "list", data=cJSON_CreateArray());
+
+	for ( it = list.begin(); it != list.end(); it++ ) {
+		cJSON_AddItemToArray(data, cJSON_CreateString(it->c_str()));
+	}
+
+	out = cJSON_Print(root);
+	cJSON_Delete(root);
+
+	printf("Content-type: application/json\r\n\r\n");
+	printf("%s\r\n", out);
+	free(out);
+}
+
+void web_factory_desribe() {
+	std::map<std::string, otProperty*>::iterator it;
+	char *out;
+	cJSON *root, *mod, *properties, *io;
+	otDataStream *ds;
+	otModule *module;
+
+	if ( strlen(ClientInfo->Query((char*)"name")) == 0 )
+		return web_error("missing name");
+
+	module = otFactory::getInstance()->create(ClientInfo->Query((char*)"name"));
+	if ( module == NULL )
+		return web_error("invalid name");
+
+	root = cJSON_CreateObject();
+	cJSON_AddNumberToObject(root, "success", 1);
+	cJSON_AddStringToObject(root, "message", "ok");
+	cJSON_AddItemToObject(root, "describe", mod=cJSON_CreateObject());
+
+	cJSON_AddStringToObject(mod, "name", module->getName().c_str());
+	cJSON_AddStringToObject(mod, "description", module->getDescription().c_str());
+	cJSON_AddStringToObject(mod, "author", module->getAuthor().c_str());
+	cJSON_AddNumberToObject(mod, "running", module->isStarted() ? 1 : 0);
+	cJSON_AddItemToObject(mod, "properties", properties=cJSON_CreateObject());
+
+	for ( it = module->properties.begin(); it != module->properties.end(); it++ ) {
+		cJSON_AddStringToObject(properties, it->first.c_str(),
+				it->second->asString().c_str());
+	}
+
+	if ( module->getInputCount() ) {
+		cJSON_AddItemToObject(mod, "inputs", io=cJSON_CreateObject());
+		for ( int i = 0; i < module->getInputCount(); i++ ) {
+			ds = module->getInput(i);
+			cJSON_AddNumberToObject(io, "index", i);
+			cJSON_AddStringToObject(io, "name", module->getInputName(i).c_str());
+			cJSON_AddStringToObject(io, "type", module->getInputType(i).c_str());
+		}
+	}
+
+	if ( module->getOutputCount() ) {
+		cJSON_AddItemToObject(mod, "outputs", io=cJSON_CreateObject());
+		for ( int i = 0; i < module->getOutputCount(); i++ ) {
+			ds = module->getOutput(i);
+			cJSON_AddNumberToObject(io, "index", i);
+			cJSON_AddStringToObject(io, "name", module->getOutputName(i).c_str());
+			cJSON_AddStringToObject(io, "type", module->getOutputType(i).c_str());
+		}
+	}
+
+	delete module;
+
+	out = cJSON_Print(root);
+	cJSON_Delete(root);
+
+	printf("Content-type: application/json\r\n\r\n");
+	printf("%s\r\n", out);
+	free(out);
+}
+
 void web_pipeline_connect() {
 	otModule *in, *out;
 	int inidx = 0, outidx = 0;
@@ -225,6 +308,8 @@ int main(int argc, char **argv) {
 	pipeline = new otPipeline();
 
 	web_server_init(&server, 7500, NULL, 0);
+	web_server_addhandler(&server, "* /factory/list", web_factory_list, 0);
+	web_server_addhandler(&server, "* /factory/describe", web_factory_desribe, 0);
 	web_server_addhandler(&server, "* /pipeline/create", web_pipeline_create, 0);
 	web_server_addhandler(&server, "* /pipeline/status", web_pipeline_status, 0);
 	web_server_addhandler(&server, "* /pipeline/connect", web_pipeline_connect, 0);
