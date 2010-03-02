00f6ca04bb232e095fa68475f7c49bfcac62e183
hansent <thomas.hansen@gmail.com>
2010-03-02 17:15:10 -0600

    fixed pymt gui.  connections work, start/stop button causes image display windows to be shown.  TODO: pymt streaming from movid

--- gui/pymt/pymt-gui.py
+++ gui/pymt/pymt-gui.py
@@ -11,8 +11,13 @@ def spline_4p( t, p_1, p0, p1, p2 ):
         + t*((4 - 3*t)*t + 1) * p1
         + (t-1)*t*t         * p2 ) / 2
 
-def make_spline(origin, destination, controll_points, resolution=10):
-    P = [origin, origin] + controll_points + [destination, destination]
+def make_spline(points, resolution, first_is_controll=False, last_is_controll=False):
+    if not first_is_controll:
+        points.insert(0,points[0])
+    if not last_is_controll:
+        points.append(points[-1])
+
+    P = points
     p = []
     for j in range( 1, len(P)-2 ):  # skip the ends
         for t in xrange( resolution ):  # t: 0 .1 .2 .. .9
@@ -21,98 +26,101 @@ def make_spline(origin, destination, controll_points, resolution=10):
             p.extend((x,y))
     return p
 
+def drawSpline(points, resolution=10, width=1):
+    drawLine(make_spline(points, resolution), width=width)
+
 
 
 class Singleton(type):
     def __init__(cls, name, bases, dict):
         super(Singleton, cls).__init__(name, bases, dict)
         cls.instance = None
- 
+
     def __call__(cls, *args, **kw):
         if cls.instance is None:
             cls.instance = super(Singleton, cls).__call__(*args, **kw)
         return cls.instance
 
 class MovidGUI(MTScatterPlane):
-    
+
     __metaclass__ = Singleton
-    
+
     def __init__(self, daemon):
         super(MovidGUI, self).__init__()
         self.daemon = daemon
-
+        self.connections = []
+        self.modules = {}
         for module in daemon.modules.values():
-            self.add_widget(ModuleNode(module, size=(200,150)) )
-            
+            self.modules[module.id] = ModuleNode(module, size=(200,150))
+            self.add_widget(self.modules[module.id] )
+
     def new_module_btn(self, m, *args):
         module = self.daemon.add_module(m)
-        self.add_widget(ModuleNode(module, size=(200,150)) )
-        
-        
-        
+        self.modules[module.id] = ModuleNode(module, size=(200,150))
+        self.add_widget(self.modules[module.id]  )
+
+    def find_connection(self, window_pos):
+        for mod in self.children:
+            if mod.collide_point(*self.to_local(*window_pos)):
+                for btn in mod.io_buttons:
+                    if btn.collide_point(*btn.to_widget(*window_pos)):
+                        return btn
+        return False
+
+
+
 class ConnectionButton(MTButton):
     def __init__(self, **kwargs):
         kwargs.setdefault('anchor_x', 'left')
         kwargs.setdefault('anchor_y', 'bottom')
         kwargs.setdefault('color', (1,1,1))
         super(ConnectionButton, self).__init__(**kwargs)
-        
+
+        self.index = kwargs['index']
+        self.module = kwargs['module']
+        self.label_obj.text = str(self.index)+":"+self.label_obj.text
+
         self.img = Image('arrow.png')
         self.height = self.img.height
         self.width = self.img.width + self.label_obj.width
-        self.userdata = {}
-        self.userdata['target'] = None
-        
+
+        self.connection_line = None
+        self.connection_color = (1,0,0)
         self.connection = None
-        
+
+    def draw(self):
+        if self.connection_line:
+            set_color(*self.connection_color)
+            drawLine(self.connection_line, width=4)
+
+    def build_line(self, src, dst):
+        return src + dst
+
+    def target_moded(self, x, y):
+        self.build_line(self.attach_pos(), )
+
     def on_touch_down(self, touch):
-        touch.userdata['target'] = None
-        touch.userdata['origin'] = None
         if self.collide_point(*touch.pos):
             touch.grab(self)
-            touch.userdata['origin'] = self
-            self.connection = touch
             return True
-         
-    def on_touch_move(self, touch):
-        if touch.grab_current == self and self == touch.userdata['target']:
-            if  not self.collide_point(*touch.pos):
-                touch.userdata['target'] = None
-        elif self.collide_point(*touch.pos) and touch.userdata['origin'] != self:
-            touch.userdata['target'] = self
-            touch.grab(self)
-            
-        
-            
-    def on_touch_up(self, touch):
-        if touch.userdata['target'] == self:
-            self.userdata['target'] = self
-            touch.userdata['origin'].connection = self
-        
-            
-    def attach_pos(self):
-        return self.x , self.y + self.img.height/2
-    def attach_offset(self):
-        return -1
-            
-    def draw(self):
-        if self.connection:
-            src = self.attach_pos()
-            dst = self.to_widget(*self.connection.pos)
-            
-            set_color(.9,.6,0)
-            if self.connection.userdata['target']:
-                set_color(0,1,0)
-            if self.connection.userdata['target'] is ConnectionButton :
-                set_color(1,1,0)
-                src = self.attach_pos()
-                dst = self.to_widget(*self.connection.to_window(self.connection.pos))
-                
 
-            d = self.attach_offset() * Vector(src).distance(dst) *0.2
-            spline = make_spline(src, dst, [(src[0]+d, src[1]), (dst[0]-d, dst[1])])
-            drawLine( spline , width=4)
-            
+    def on_touch_move(self, touch):
+        if touch.grab_current == self:
+            self.connection_line = self.build_line(self.attach_pos(), touch.pos)
+            target = MovidGUI().find_connection(self.to_window(*touch.pos))
+            if self.is_valid_target(target):
+                self.connection_color = (0,1,0)
+            else:
+                self.connection_color = (1,0,0)
+            return True
+
+    def on_touch_up(self, touch):
+        if touch.grab_current == self:
+            GUI = MovidGUI()
+            target = GUI.find_connection(self.to_window(*touch.pos))
+            if self.is_valid_target(target):
+                self.connect(target)
+                self.connection_line = None
 
 
 class InputButton(ConnectionButton):
@@ -123,64 +131,81 @@ class InputButton(ConnectionButton):
         self.label_obj.x += self.img.width
         self.label_obj.draw()
         super(InputButton, self).draw()
-        
+    def attach_pos(self):
+        return self.x , self.y + self.img.height/2
+    def attach_offset(self):
+        return -0.3
+    def is_valid_target(self, io_button):
+        return isinstance(io_button, OutputButton)
+    def connect(self, target):
+        server = MovidGUI().daemon
+        server.connect_module_by_index(target.module, target.index, self.module, self.index)
+        target.parent.parent.connections = [(target.index, self.index, self.module)]
+
 class OutputButton(ConnectionButton):
     def draw(self):
         self.label_obj.pos = self.pos
-        self.label_obj.draw()    
+        self.label_obj.draw()
         self.img.pos = self.pos
         self.img.x += self.label_obj.width
         self.img.draw()
         super(OutputButton, self).draw()
-        
     def attach_pos(self):
         return (self.x + self.img.width + self.label_obj.width, self.y + self.img.height/2)
     def attach_offset(self):
-        return 1
-
+        return 0.3
+    def is_valid_target(self, io_button):
+        return isinstance(io_button, InputButton)
+    def connect(self, target):
+        server = MovidGUI().daemon
+        server.connect_module_by_index(self.module, self.index, target.module, target.index)
+        self.parent.parent.connections =[(self.index, target.index, target.module)]
 
 class ModuleNode(MTScatterWidget):
-    
+
     def __init__(self, module, **kwargs):
         super(ModuleNode, self).__init__(**kwargs)
         self.module = module
-        
+
+        self.connections = []
+        self.io_buttons = []
+        self.input_buttons = []
+        self.output_buttons = []
+
+        #add input connections
         input_box = MTBoxLayout(orientation='vertical')
         for inp in self.module.inputs:
-            btn = InputButton(label=inp['name'])
+            btn = InputButton(label=inp['name'], index=inp['index'], module=self.module.id,)
+            self.io_buttons.append(btn)
+            self.input_buttons.append(btn)
             input_box.add_widget(btn)
         self.add_widget(input_box)
-        
+
+        #add output connections
         output_box = MTBoxLayout(orientation='vertical')
         for out in self.module.outputs:
-            btn = OutputButton(label=out['name'])
+            btn = OutputButton(label=out['name'], module=self.module.id, index=out['index'])
+            for indx, module_id in out['observers'].items():
+                self.connections.append((out['index'], indx, module_id))
+            self.io_buttons.append(btn)
+            self.output_buttons.append(btn)
             output_box.add_widget(btn)
-        output_box.x = self.width - output_box.content_width
+        output_box.x = self.width - output_box.width
         self.add_widget(output_box)
-        
+
+        #make name and property labels
         self.label = Label("%s"% (self.module.id))
         self.property_labels = {}
         for key, val in self.module.properties.items():
             label = Label(key+"="+str(val))
             self.property_labels[key] = label
-            
-    def collide_point(self, x, y):
-        local_coords = self.to_local(x, y)
-        if local_coords[0] > 0 and local_coords[0] < self.width \
-           and local_coords[1] > 0 and local_coords[1] < self.height:
-            return True
-        else:
-            for c in self.children:
-                if c.collide_point(*local_coords):
-                    return True
-            return False
-            
-            
+
+
     def update_info(self):
         self.label.text = "%s"% (self.module.id)
         for key, val in self.module.properties.items():
             self.property_labels[key].text = key+"="+str(val)
-            
+
     def draw(self):
         set_color(0,0,0,0.5)
         drawRectangle(size=self.size)
@@ -188,37 +213,52 @@ class ModuleNode(MTScatterWidget):
             glTranslate(0, self.height-self.label.height-5, 0)
             self.label.draw()
             drawLine([0,0,self.width, 0], width=1)
-        
 
-
-
-    
-
-daemon = MovidDaemon()
-
-if not len(daemon.modules):
-    daemon.add_module("Camera", "cam")
-    daemon.add_module("ImageDisplay", "display")
-
-
-
-root = MTWidget()
-gui = MovidGUI(daemon)
-toolbar = MTBoxLayout(orientation='vertical')
-
-for m in daemon.available_modules:
-    button = MTButton(label=m, size=(150,40))
-    button.push_handlers( on_release=curry(gui.new_module_btn, m) )
-    toolbar.add_widget(button)
-toolbar.add_widget(MTLabel(label="Modules:"))
+            for l in self.property_labels.values():
+                glTranslate(0, -l.height-2, 0)
+                l.draw()
+        for c in self.connections:
+            src = self.output_buttons[int(c[0])]
+            target = MovidGUI().modules[c[2]].input_buttons[int(c[1])]
+            x,y = self.to_widget(*src.to_window(*src.attach_pos()))
+            x1,y1 = self.to_widget(*target.to_window(*target.attach_pos()))
+            set_color(0,1,0)
+            drawLine([x,y,x1,y1], width=4)
 
 
 
 
-container = MTWidget()
-container.add_widget(gui)
-root.add_widget(container)
-root.add_widget(toolbar)
+
+if __name__ == "__main__":
+    #Open Tracker Gui
+    daemon = MovidDaemon()
+    gui = MovidGUI(daemon)
+
+    #add module buttons
+    toolbar = MTBoxLayout(orientation='vertical')
+    for m in daemon.available_modules:
+        button = MTButton(label=m, size=(150,40))
+        button.push_handlers( on_release=curry(gui.new_module_btn, m) )
+        toolbar.add_widget(button)
+    toolbar.add_widget(MTLabel(label="Modules:"))
+
+    start_btn = MTButton(label="start", size=(150,100))
+    def start_stop(*args):
+        if start_btn.label == "start":
+            daemon.start()
+            start_btn.label = "stop"
+        else:
+            daemon.stop()
+            start_btn.label = "start"
+    start_btn.push_handlers(on_press=start_stop)
+    toolbar.add_widget(start_btn)
+
+    #add so that toolar stays on top
+    root = MTWidget()
+    container = MTWidget()
+    container.add_widget(gui)
+    root.add_widget(container)
+    root.add_widget(toolbar)
 
 
-runTouchApp(root)
+    runTouchApp(root)
--- gui/pymt/pyot.py
+++ gui/pymt/pyot.py
@@ -5,20 +5,20 @@ class otModulePropertiesProxy(object):
     def __init__(self, module_proxy, properties):
         self._module_proxy = module_proxy
         self.cache = properties
-        
+
     def __getitem__(self, key):
         return self.cache[key]
-    
+
     def __setitem__(self, key, val):
         self._module_proxy.set_property(key, val)
         self.cache[key] = val
-        
+
     def keys(self):
         return self.cache.keys()
-        
+
     def items(self):
         return self.cache.items()
-        
+
     def __repr__(self):
         return str(self.cache)
 
@@ -28,47 +28,48 @@ class otModuleProxy(object):
         self.daemon = daemon
         self._id = id
         self._type = type
-        
+
         property_dict = self.daemon.describe(self.type)['properties']
+        property_dict['id'] = id
         self.properties = otModulePropertiesProxy(self, property_dict)
-        
+
     def _get_id(self):
         return self._id
     def _set_id(self, id):
         self.properties['id'] = id
         self.daemon.rename_module(self._id, id)
     id = property(_get_id, _set_id)
-    
+
     def _get_type(self):
         return self._type
     type = property(_get_type)
-    
+
     def _get_description(self):
         return self.daemon.describe(self.type)['description']
     description = property(_get_description)
-    
+
     def _get_inputs(self):
-        if self.daemon.describe(self.type).has_key('inputs'):
-            print self.daemon.describe(self.type)['inputs']
-            return self.daemon.describe(self.type)['inputs']
+        module = self.daemon.status['modules'][self.id]
+        if module.has_key('inputs'):
+            return module['inputs']
         else:
             return []
     inputs = property(_get_inputs)
-    
+
     def _get_outputs(self):
-        if self.daemon.describe(self.type).has_key('outputs'):
-            print self.daemon.describe(self.type)['outputs']
-            return self.daemon.describe(self.type)['outputs']
+        module = self.daemon.status['modules'][self.id]
+        if module.has_key('outputs'):
+            return module['outputs']
         else:
             return []
     outputs = property(_get_outputs)
-    
+
     def get_property(self, property_name):
         return self.daemon.get_module_property(self.id, property_name)
 
     def set_property(self, property_name, val):
         self.daemon.set_module_property(self.id, property_name, val)
-    
+
     def __repr__(self):
         return "<"+self.type + "Module  properties=" + str(self.properties)+">"
 
@@ -79,32 +80,34 @@ class MovidDaemon(object):
         self.daemon = daemon
         self.update_status()
 
-        
+
     def update_status(self):
         self.modules = {}
         for id, module in self.status['modules'].items():
+            print 'reading mocdules:', id, module['name']
             self.modules[id] = otModuleProxy(self, id, module['name'])
-        
-        
+
+
     def remote_call(self, address, selection=None):
         url = self.daemon + address
+        #print "calling: ", url
         connection = urllib.urlopen(url)
         response = connection.read()
         connection.close()
         result =  json.loads( response )
-        
+
         if result['success'] == 0:
-            raise Exception("Movid Error: \n Message: %s"% result['message'])
-        
+            raise Exception("OpenTracker Error: \n Message: %s"% result['message'])
+
         if selection:
             return result[selection]
         return result
-        
-        
+
+
     def _list_modules(self):
         return self.remote_call('/factory/list', 'list')
     available_modules = property(_list_modules)
-    
+
     def _get_status(self):
         return self.remote_call('/pipeline/status', 'status')
     status = property(_get_status)
@@ -117,25 +120,26 @@ class MovidDaemon(object):
 
     def start(self):
         self.remote_call('/pipeline/start')
-        
+
     def stop(self):
         self.remote_call('/pipeline/stop')
-        
+
     def quit(self):
         self.remote_call('/pipeline/quit')
-        
+
     def rename_module(self, module_id, new_id):
-        print "renaming ", new_id
+        #print "renaming ", new_id
         if not self.modules.has_key(module_id):
             raise Excpetion("Can't rename!  No module named: %s"%module_id)
         module = self.modules[module_id]
         module._id = new_id
         del self.modules[module_id]
         self.modules[new_id] = module
-        
-        
+
+
     def add_module(self, objectname, id=None):
         new_id = self.remote_call('/pipeline/create?objectname='+objectname, 'message')
+        #print "Created:", new_id, "    from:", objectname
         module = otModuleProxy(self, new_id, objectname)
         self.modules[module.id] = module
         if id:
@@ -146,11 +150,13 @@ class MovidDaemon(object):
     def get_module_property(self, module_id, property_name):
         url = '/pipeline/get?objectname=%s&name=%s' % (module_id, property_name)
         return self.remote_call(url)
-        
+
     def set_module_property(self, module_id, property_name, value):
+        #print "setting" , module_id, property_name, value
         url = '/pipeline/set?objectname=%s&name=%s&value=%s' % (module_id, property_name, str(value))
         return self.remote_call(url)
-        
+
     def connect_module_by_index(self, out_module, out_port, in_module, in_port):
-        url = '/pipeline/connect?out=%s&in=%s&outidx=%s&inidx=%s' % (out_module, in_module, str(out_port), str(in_port))
+        url = '/pipeline/connect?in=%s&out=%s&inidx=%s&outidx=%s' % (in_module,out_module, str(in_port), str(out_port))
+        #print url
         self.remote_call(url)
