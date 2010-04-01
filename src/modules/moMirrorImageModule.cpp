7ad381981289aed8cc8c968c0b426e87090cbe6c
hansent <thomas.hansen@gmail.com>
2010-04-01 11:09:48 -0500

    add QPL license and license headers, add AUTHORS file and update README

--- src/modules/moMirrorImageModule.cpp
+++ src/modules/moMirrorImageModule.cpp
@@ -1,3 +1,21 @@
+/***********************************************************************
+ ** Copyright (C) 2010 Movid Authors.  All rights reserved.
+ **
+ ** This file is part of the Movid Software.
+ **
+ ** This file may be distributed under the terms of the Q Public License
+ ** as defined by Trolltech AS of Norway and appearing in the file
+ ** LICENSE included in the packaging of this file.
+ **
+ ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ **
+ ** Contact info@movid.org if any conditions of this licensing are
+ ** not clear to you.
+ **
+ **********************************************************************/
+
+
 #include "moMirrorImageModule.h"
 #include "../moLog.h"
 #include "cv.h"
