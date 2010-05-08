da6806d3602d36f7d5e8054ad9aa22ad45856c72
Christopher Denter <dennda -O- the-space-station -o- com>
2010-05-08 02:11:44 +0200

    PeakFinder: Add support for only giving back the best X peaks

--- src/modules/moPeakFinderModule.cpp
+++ src/modules/moPeakFinderModule.cpp
@@ -23,15 +23,22 @@
 
 MODULE_DECLARE(PeakFinder, "native", "PeakFinder Description");
 
+typedef std::pair<double, moPoint> doubleToPoint;
+
 moPeakFinderModule::moPeakFinderModule() : moImageFilterModule(){
 
 	MODULE_INIT();
 
 	this->properties["min_radius"] = new moProperty(5.);
 	this->properties["max_radius"] = new moProperty(10.);
+	this->properties["max_peaks"] = new moProperty(10);
 }
 
 moPeakFinderModule::~moPeakFinderModule() {
+}
+
+bool _sort_pred(const doubleToPoint &left, const doubleToPoint &right) {
+	return left.first > right.first;
 }
 
 void moPeakFinderModule::applyFilter(IplImage *src) {
@@ -43,18 +50,30 @@ void moPeakFinderModule::applyFilter(IplImage *src) {
 	double max = this->property("max_radius").asDouble();
 	double cur_val;
 	char *data = src->imageData;
-	std::vector<std::pair<double, moPoint> > peaks;
+	std::vector<doubleToPoint> peaks;
 
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
 			cur_val = ((int) data[j * step + i]);
 			if ((min < cur_val) && (cur_val < max)) {
-				peaks.push_back(std::pair<double, moPoint>(cur_val, (moPoint) {i, j}));
+				peaks.push_back(doubleToPoint(cur_val, (moPoint) {i, j}));
 			}
 		}
 	}
+
+	// If the max_peaks option was set, we only give back the $max_peaks strongest peaks.
+	int max_peaks = this->property("max_peaks").asInteger();
+	if (max_peaks != 0 && max_peaks < peaks.size()) {
+		std::sort(peaks.begin(), peaks.end(), _sort_pred);
+		// We do so by erasing the weak peaks.
+		peaks.erase(peaks.begin()+max_peaks, peaks.end());
+	}
+
+	// Draw the peaks that we've found so we can see if it makes sense
 	cvSet(this->output_buffer, cvScalar(0, 0, 0));
+	//std::cout << "---" << std::endl;
 	for (unsigned int i = 0; i < peaks.size(); i++) {
+		//std::cout << peaks[i].first << std::endl;
 		// Coordinate system is flipped
 		CvPoint p = cvPoint(cvRound(peaks[i].second.x),
 							cvRound(peaks[i].second.y));
