0d74c4dfada7ed8afe91188198225ab958fa0ec3
Christopher Denter <dennda@the-space-station.com>
2010-05-10 09:19:03 +0200

    ContourFinder: Now correctly finds the start & endpoints (aka fingertips) of the contour with the largest area in the image. Also still finds some false fingertips

--- src/modules/moContourFinderModule.cpp
+++ src/modules/moContourFinderModule.cpp
@@ -21,6 +21,7 @@
 #include "../moLog.h"
 #include "cv.h"
 
+// XXX This is currently rather the scratchspace for a fingertip finder...
 MODULE_DECLARE(ContourFinder, "native", "ContourFinder finds contours in an image");
 
 moContourFinderModule::moContourFinderModule() : moImageFilterModule(){
@@ -37,11 +38,43 @@ moContourFinderModule::~moContourFinderModule() {
 void moContourFinderModule::applyFilter(IplImage *src) {
 	// Create a copy since cvFindContours will manipulate its input
 	cvCopy(src, this->output_buffer);
-	CvSeq* contours = 0;
-	cvFindContours(this->output_buffer, this->storage, &contours);
+	CvSeq *contours = 0;
+	cvFindContours(this->output_buffer, this->storage, &contours, sizeof(CvContour), CV_RETR_CCOMP);
 	cvZero(this->output_buffer);
-	if(contours)
+	if(contours) {
+		// Find the exterior contour (i.e. the hand has to be white) that has the greatest area
+		CvSeq *max_cont = contours, *cur_cont = contours;
+		double area, max_area = 0;
+		while (cur_cont != 0) {
+			area = cvContourArea(cur_cont);
+			area *= area;
+			if (area >= max_area) {
+				max_area = area;
+				max_cont = cur_cont;
+			}
+			cur_cont = cur_cont->h_next;
+		}
+		contours = max_cont;
+
 		cvDrawContours(this->output_buffer, contours, cvScalarAll(255), cvScalarAll(255), 100);
+
+		// Compute the convex hull of the contour
+		CvMemStorage *hull_storage = cvCreateMemStorage(0);
+		CvSeq* hull = 0;
+		hull = cvConvexHull2(contours, hull_storage, CV_CLOCKWISE, 0);
+
+		// Compute the convexity defects of the convex contour with respect to the convex hull.
+		// The fingertips are at the start and endpoints of the defects
+		CvMemStorage *defects_storage = cvCreateMemStorage(0);
+		CvSeq* defects = 0;
+		defects = cvConvexityDefects(contours, hull, defects_storage);
+
+		for(int i=0; i<defects->total; ++i) {
+			CvConvexityDefect* d = (CvConvexityDefect*)cvGetSeqElem (defects, i);
+			int radius = cvRound(10);
+			cvCircle(this->output_buffer, *(d->start), radius, CV_RGB(255, 255, 255), -1);
+		}
+	}
 }
 
 
